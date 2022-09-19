5c04afeffa8f61749cfcd834c72ad16b4ad72a92
David Carrillo <102185754+DCarrillo13@users.noreply.github.com>
2022-09-19 18:26:20 +0000

    Configuration and test cases

--- RecurseiveMethod.java
+++ RecurseiveMethod.java
@@ -1,31 +1,28 @@
+import java.awt.desktop.UserSessionEvent.Reason;
+
 class RecurseiveMethod {
 
-  private static int test = 0;
-  public static void RecurseiveMethod(int x){
+  public static int test = 0;
+
+  public static void RecurseiveMethod(int x) {
     test = x;
   }
 
-  public static boolean isRecursive(int x){
-    if(x>0){
-      return true;
-    } else return false;
-  }
-
   public static String reverseStringRecursive(int x, String s) {
-    //below method calling is for Testing if the method is being called recursivlly, please do not touch.
+    // below method calling is for Testing if the method is being called
+    // recursivlly, please do not touch.
     RecurseiveMethod(x);
 
-   /*  TODO: Write the body of the method in a recursive approach.
-    *   When calling reverseStringRecursive(...) please increment `int x`
-    *   as `++x` in the parameters to help test for recursion.
-    */
-  }
+    /*
+     * TODO: Write the body of the method in a recursive approach.
+     * When calling reverseStringRecursive(...) please increment `int x`
+     * as `++x` in the parameters to help test for recursion.
+     */
 
+  }
 
   public static void main(String[] args) {
     String input = "Hello World";
-    reverseStringRecursive(x, s);
-
-
+    reverseStringRecursive(test, input);
   }
 }
