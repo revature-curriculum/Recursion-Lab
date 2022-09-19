cfb81ada2b33f5e8496a469e0e4717c6e22e06c8
David Carrillo <david.carrillo@revature.com>
2022-09-19 06:52:41 -0700

    edit lab

--- RecurseiveMethod.java
+++ RecurseiveMethod.java
@@ -1,7 +1,6 @@
 class RecurseiveMethod {
 
   private static int test = 0;
-  
   public static void RecurseiveMethod(int x){
     test = x;
   }
@@ -15,17 +14,20 @@ class RecurseiveMethod {
   public static String reverseStringRecursive(int x, String s) {
     //below method calling is for Testing if the method is being called recursivlly, please do not touch.
     RecurseiveMethod(x);
-    //TODO: complete the recursive method while incrementing `int x` as `++x`
 
-
-
+   /*  TODO: Write the body of the method in a recursive approach.
+    *   When calling reverseStringRecursive(...) please increment `int x`
+    *   as `++x` in the parameters to help test for recursion.
+    */
   }
 
 
   public static void main(String[] args) {
-    String str = "Hello World";
-    System.out.println("Output: " + reverseStringRecursive(test, str));
-    System.out.println("Is reverseStringRecursive() recursive? - " + isRecursive(test));
+    String input = "Hello World";
+    System.out.println();
+    System.out.println(reverseStringRecursive(test, input));
+    System.out.println("Is reverseStringRecursive() recursive? - "+isRecursive(test));
+    System.out.println();
 
   }
 }
